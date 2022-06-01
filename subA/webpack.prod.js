025a8df4f9cc63e037770fdc0651b170c3dc8342
prifina-admin <tero@prifina.com>
2022-06-01 10:13:31 +0300

    test

---
+++ subA/webpack.prod.js
@@ -0,0 +1,21 @@
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
+//const TerserPlugin = require("terser-webpack-plugin");
+
+module.exports = merge(common, {
+  mode: "production",
+  output: {
+    publicPath: "/mfe-app2/",
+    clean: true,
+  },
+  /*
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        extractComments: false,
+      }),
+    ],
+  },
+  */
+});
